02e4951f3d77812ce21f65bc236a9b3f47522910
Siksna <ulfsssiksna@gmail.com>
2023-03-29 09:21:11 +0300

    Izveidots pirmais skripts

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour {
+/* FPS SKAITITAJS
+ * int kadrs=0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Starteta programma");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		kadrs++;
+		Debug.Log ("Kadrs: "+kadrs);
+	}*/
+
+
+
+	//Mainígais, kurá ieglabás tekstu no input field
+	private string teksts;
+	private string[] varianti = {"Sveiki","Atá","Uzredześanos","Adios","Jauku dienu","Prieks tevi redzet"};
+	//Uzglaba random nogenereto skaitli
+	int nejausais;
+	//Lauks no kura ievada tekstu
+	public GameObject ievadesLauks;
+	//Text kura attelos tekstu
+	public GameObject tekstaAttelosana;
+
+	public void UzglabatTekstu(){
+		nejausais = Random.Range (0,varianti.Length);
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosana.GetComponent<Text> ().text = varianti [nejausais]+teksts.ToUpper()+"!";
+	}
+}
